7fc197423aa206a658e6152008017e6598d5a6a5
Vitoria <102309048+FelipeMYung@users.noreply.github.com>
2025-01-04 19:36:30 -0300

    header section 1 base

---
+++ header.php
@@ -0,0 +1,28 @@
+     <header>
+        <section id="header-nav-dados">
+            <div id="logo">
+                <img src="" alt="">
+            </div>
+            <div id="localizacao">
+                <button>Atualizar ceep</button>
+            </div>
+            <div id="barra-pesquisa">
+                <form action="" method="get">
+                    <select name="Oi" id="">
+                        <option value="">teste</option>
+                        <option value="">dpos</option>
+                    </select>
+                    <input type="search">
+                    <input type="submit" value="">
+                </form>
+            </div>
+            <div id="login"></div>
+            <div id="devolução">
+                <button>devolução</button>
+            </div>
+            <div id="carrinho">
+                <button>carrinho</button>
+            </div>
+        </section>
+        <section id="header-nav-setores"></section>
+    </header>
